e95f09e541007b17146ddeae02188086ca8ed995
Dayvon Allen <dayvonallen@Dayvons-Mini.attlocal.net>
2021-03-10 12:55:31 -0600

    made updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,20 @@
+
+loadWeb3 = async () => {
+  // modern web browser
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable();
+  }
+  // legacy web browsers
+  else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider);
+  } else {
+    window.alert(
+      "Non-Ethereum browser detected, You should consider trying MetaMask"
+    );
+  }
+};
+
 function App() {
   return (
     <div className="App">
